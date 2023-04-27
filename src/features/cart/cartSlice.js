8e08b485e2917110135ccbea9468399c9e1e13c1
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-27 16:35:54 +0430

    Setup redux state slicer.

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cartItems: [],
+    amount: 0,
+    total: 0,
+    isLoading: true
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+
+});
+
+// console.log(cartSlice);
+
+export default cartSlice.reducer;
+
