1b7e74ee702ea2c69540270d8c2bf9cdcea895d9
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-27 17:19:05 +0430

    Install CSS Pre-processor & add Sass file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 const container = document.getElementById('root');
