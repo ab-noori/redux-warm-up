c6dc26e61db2823aa3d6116632c61c29f172b762
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-27 16:37:02 +0430

    Add react-redux starter files.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
